3e5824ff9918b0d8d85d435ddfb5190188d4e7fb
JulioLms4 <160614521+JulioLms4@users.noreply.github.com>
2024-04-03 11:40:00 -0300

    Add files via upload

---
+++ javao/script.js
@@ -0,0 +1,8 @@
+function funcaoMudarTamanho() {
+    document.getElementById('test').style.fontSize = "35px";
+}
+
+function conta() {
+    document.getElementById('test').innerHTML = 50 + 10 * 2;
+}
+
